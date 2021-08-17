e78f2f705ca605aef9d13df0f49a0d9bf5cd7a66
pedro.gomes <pedro.gomes@luizalabs.com>
2021-08-17 08:46:53 -0300

    Use hilt to inject MainViewModel.kt

--- app/src/main/java/com/pedroimai/sandbox/ui/main/MainFragment.kt
+++ app/src/main/java/com/pedroimai/sandbox/ui/main/MainFragment.kt
@@ -1,20 +1,21 @@
 package com.pedroimai.sandbox.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.pedroimai.sandbox.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
+    private val viewModel: MainViewModel by viewModels()
 
     companion object {
         fun newInstance() = MainFragment()
     }
-
-    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +24,8 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         // TODO: Use the ViewModel
     }
-
 }
