864480cb559d6c3173d9bc7c404b2a4c886d7934
pedro.gomes <pedro.gomes@luizalabs.com>
2022-02-06 22:19:18 -0300

    Add CharacterRepository.kt

--- characters/src/main/java/com/pedroimai/characters/CharacterRepository.kt
+++ characters/src/main/java/com/pedroimai/characters/CharacterRepository.kt
@@ -1,12 +1,25 @@
 package com.pedroimai.characters
 
+import com.pedroimai.shared.domain.CharactersPayload
+import com.pedroimai.shared.network.MarvelApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
+
 class CharacterRepository @Inject constructor() {
-    fun getCharacters() = listOf(
-        CharacterModel(id = "1", name = "Spiderman"),
-        CharacterModel(id = "2", name = "Wolverine"),
-        CharacterModel(id = "3", name = "Iron-Man"),
-        CharacterModel(id = "4", name = "Thanos"),
-    )
+    private val marvelApi = MarvelApi.Impl() //todo inject marvelApi
+
+    fun getCharacters(): Flow<Result<CharactersPayload.Characters>> = flow {
+        emit(
+            Result.success(marvelApi.getCharacters().data) as Result<CharactersPayload.Characters>
+        )
+
+    }.onStart {
+        emit(Result.loading<CharactersPayload.Characters>() as Result<CharactersPayload.Characters>)
+    }
+        .catch { exception ->
+            emit(Result.failed<CharactersPayload.Characters>(exception) as Result<CharactersPayload.Characters>)
+        }
+        .flowOn(Dispatchers.IO)
 }
---
+++ characters/src/main/java/com/pedroimai/characters/Result.kt
@@ -0,0 +1,13 @@
+package com.pedroimai.characters
+
+sealed class Result<T> {
+    class Loading<T> : Result<T>()
+    data class Success<T>(val data: T) : Result<T>()
+    data class Failed<T>(val error: Throwable) : Result<T>()
+
+    companion object {
+        fun <T> loading() = Loading<T>()
+        fun <T> success(data: T) = Success(data)
+        fun <T> failed(error: Throwable) = Failed<T>(error)
+    }
+}
