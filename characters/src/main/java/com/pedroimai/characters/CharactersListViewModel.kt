a531e6f80d5ef139b6521a124187cd771bd2fbfe
pedro.gomes <pedro.gomes@luizalabs.com>
2022-02-06 22:20:27 -0300

    CharactersListViewModel improvements

--- characters/src/main/java/com/pedroimai/characters/CharactersListViewModel.kt
+++ characters/src/main/java/com/pedroimai/characters/CharactersListViewModel.kt
@@ -1,11 +1,17 @@
 package com.pedroimai.characters
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.pedroimai.shared.domain.CharactersPayload
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersListViewModel @Inject constructor(private val repository: CharacterRepository) :
     ViewModel() {
-    fun getCharacters() = repository.getCharacters()
+
+    val characters: LiveData<Result<CharactersPayload.Characters>>
+        get() = repository.getCharacters().asLiveData(viewModelScope.coroutineContext)
 }
