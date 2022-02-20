2791ef1e865767bef56ec01ab67c7f6da5586d30
pedro.gomes <pedro.gomes@luizalabs.com>
2022-02-20 17:51:24 -0300

    Add comics module

---
+++ comics/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = Env.compileSdkVersion
+    buildToolsVersion = Env.buildToolsVersion
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    defaultConfig {
+        minSdk = Env.minSdkVersion
+        targetSdk = Env.targetSdkVersion
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+dependencies {
+    implementation(Dependencies.kotlin)
+    implementation(Dependencies.appcompat)
+
+    //layout
+    implementation(Dependencies.material)
+    implementation(Dependencies.constraintlayout)
+
+    //ktx
+    implementation(Dependencies.ktx.activity)
+    implementation(Dependencies.ktx.fragment)
+    implementation(Dependencies.ktx.core)
+    implementation(Dependencies.ktx.lifecycle.viewmodel)
+    implementation(Dependencies.ktx.lifecycle.runtime)
+
+    //hilt
+    implementation(Dependencies.hilt.core)
+    kapt(Dependencies.hilt.compiler)
+    testImplementation(Dependencies.test.junit)
+    androidTestImplementation(Dependencies.test.androidJunit)
+    androidTestImplementation(Dependencies.test.espresso)
+
+    implementation(project(Dependencies.modules.shared))
+}
---
+++ comics/src/main/java/com/pedroimai/comics/ComicsViewModel.kt
@@ -0,0 +1,7 @@
+package com.pedroimai.comics
+
+import androidx.lifecycle.ViewModel
+
+class ComicsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
