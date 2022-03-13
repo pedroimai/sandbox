c3fccbff58ea54c36a6f01d83f6b6e3dd619d285
pedro.gomes <pedro.gomes@luizalabs.com>
2022-03-13 10:34:50 -0300

    Improved Comics screen UI

--- comics/src/main/java/com/pedroimai/comics/ComicsFragment.kt
+++ comics/src/main/java/com/pedroimai/comics/ComicsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -19,7 +20,9 @@ class ComicsFragment : Fragment() {
     ): View =
         ComposeView(requireContext()).apply {
             setContent {
-                ComicsScreen(viewModel)
+                MaterialTheme {
+                    ComicsScreen(viewModel)
+                }
             }
         }
 }
--- comics/src/main/java/com/pedroimai/comics/ComicsUi.kt
+++ comics/src/main/java/com/pedroimai/comics/ComicsUi.kt
@@ -1,21 +1,20 @@
 package com.pedroimai.comics
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import com.pedroimai.shared.domain.ComicsPayload
@@ -35,13 +34,31 @@ fun ComicsScreen(viewModel: ComicsViewModel) {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun ComicsList(list: ComicsPayload.Comics) =
-    LazyColumn {
+fun ComicsList(list: ComicsPayload.Comics) {
+    val context = LocalContext.current
+
+    return LazyColumn(Modifier.fillMaxWidth()) {
         items(items = list.comics) { comic ->
-            Text(text = comic.name)
+            Surface(onClick = {
+                Toast.makeText(
+                    context,
+                    "${comic.name} clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }) {
+                Text(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 16.dp)
+                        .fillMaxWidth(),
+                    fontSize = 16.sp,
+                    text = comic.name
+                )
+            }
         }
     }
+}
 
 @Composable
 fun Loading() =
